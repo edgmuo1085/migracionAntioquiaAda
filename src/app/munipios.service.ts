caf47c87482851a3e0556c31c32e805494194535
Edgar Muñoz <edgmuo1085@gmail.com>
2023-06-23 17:20:10 -0500

    se agrega ip publica

--- src/app/munipios.service.ts
+++ src/app/munipios.service.ts
@@ -8,6 +8,11 @@ import { ResponseApiRest } from './interfaces/api-rest.interface';
   providedIn: 'root',
 })
 export class MunipiosService {
+  ipPub: string = 'http://141.148.95.161';
+  ipPrepro: string = 'http://10.0.200.131:8080';
+  ipDev: string = 'http://10.0.200.160:8080';
+  ipDom: string = 'http://serviciosdigitales.catastroantioquia.co';
+
   constructor(private http: HttpClient) {}
 
   getCities(): Observable<ResponseMunicipios> {
@@ -24,7 +29,7 @@ export class MunipiosService {
     const data = { codInterno: codInterno };
     const json = window.btoa(JSON.stringify(data));
     const headers = new HttpHeaders().set('content-type', 'text/plain').set('appCode', '6');
-    return this.http.get<ResponseApiRest>(`http://10.0.200.131:8080/ApiRest/param/consultafiltro/${json}`, {
+    return this.http.get<ResponseApiRest>(`${this.ipDom}/ApiRest/param/consultafiltro/${json}`, {
       headers,
     });
   }
